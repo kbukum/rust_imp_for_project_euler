69d0e550c58c7a1a55af1b4e61a6ad3b47b786fe
Kamil BUKUM <kamilbukum@gmail.com>
2022-04-24 19:20:35 -0400

    - Added "Largest palindrome product" problem solution.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod multiples;
 mod arrays;
 mod fibonacci;
 mod prime_factor;
+mod palindrome;
 
 /// This global variable refers to the title for the application.
 static  APPLICATION_NAME: &str = "Implementation of the Project Euler Problems in Rust!";
@@ -18,10 +19,6 @@ pub fn print_header(header: &str, fn_list: Vec<(&str, fn())>) {
 }
 
 fn main() {
-    multiples::find_the_sum_of_multiples_of_the_factors();
-    fibonacci::find_sum_of_even_fib_numbers();
-    prime_factor::find_the_largest_prime_factor();
-
     println!("{}", APPLICATION_NAME);
 
     print_header("Multiples", vec![
@@ -34,5 +31,9 @@ fn main() {
 
     print_header("Prime Factor", vec![
         ("Find the largest prime factor",  prime_factor::find_the_largest_prime_factor),
+    ]);
+
+    print_header("Largest palindrome product", vec![
+        ("Find the largest palindrome of the products",  palindrome::find_the_largest_palindrome_product),
     ]);
 }
---
+++ src/palindrome.rs
@@ -0,0 +1,86 @@
+/// Largest palindrome product
+/// A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+/// Find the largest palindrome made from the product of two 3-digit numbers.
+pub(crate) fn find_the_largest_palindrome_product() {
+    let product_digit: u32 = 3;
+    let largest_palindrome_product = _find_the_largest_palindrome_product(product_digit);
+    match largest_palindrome_product {
+        Some(num) => {
+            let ((a, b), c) = num;
+            println!("The largest palindrome for {} digit products, {}x{} = {} ", 3, a, b, c);
+        },
+        None => {
+            println!("There is no largest palindrome for {} digit products is", 3);
+        }
+    }
+}
+
+fn _find_the_largest_palindrome_product(product_digit: u32) -> Option<((u64,u64), u64)> {
+    if product_digit <= 0 {
+        return None
+    }
+    let d : u64= 10;
+    let max_product_value =  d.pow(product_digit as u32);
+    let min_product_value =  d.pow(product_digit as u32 - 1);
+    let mut max_value =  max_product_value * max_product_value - 1;
+    while max_value > min_product_value {
+        if _is_palindrome(max_value) {
+            let  mut p1 = max_product_value - 1;
+            while p1 > min_product_value {
+                if max_value % p1 == 0 {
+                    let p2 = max_value/p1;
+                    if p2 < max_product_value && p2 > min_product_value {
+                        return Some(((p1, p2), max_value))
+                    }
+                }
+                p1 -= 1;
+            }
+        }
+        max_value -= 1;
+    }
+    return None
+}
+
+fn _is_palindrome(num: u64) -> bool {
+    let s = num.to_string();
+    let bytes = s.as_bytes();
+    for start in 0..bytes.len() {
+        let end = bytes.len()-1-start;
+        if start >= end {
+            return true
+        }
+        if bytes[start] != bytes[end] {
+            return false
+        }
+    }
+    false
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::palindrome;
+
+    #[test]
+    fn find_the_largest_palindrome_product() {
+
+    }
+
+    #[test]
+    fn _is_palindrome() {
+        assert_eq!(palindrome::_is_palindrome(1), true);
+        assert_eq!(palindrome::_is_palindrome(9), true);
+        assert_eq!(palindrome::_is_palindrome(10), false);
+        assert_eq!(palindrome::_is_palindrome(11), true);
+        assert_eq!(palindrome::_is_palindrome(12), false);
+        assert_eq!(palindrome::_is_palindrome(99), true);
+        assert_eq!(palindrome::_is_palindrome(100), false);
+        assert_eq!(palindrome::_is_palindrome(101), true);
+        assert_eq!(palindrome::_is_palindrome(111), true);
+        assert_eq!(palindrome::_is_palindrome(199), false);
+        assert_eq!(palindrome::_is_palindrome(123321), true);
+        assert_eq!(palindrome::_is_palindrome(1234321), true);
+        assert_eq!(palindrome::_is_palindrome(123321123321), true);
+        assert_eq!(palindrome::_is_palindrome(12343211234321), true);
+    }
+}
+
